99886978c39570269ba6551275d7de03e552b053
emmakevin <168530110+emmakevin@users.noreply.github.com>
2024-04-30 22:15:30 +1200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,6 @@ import Footer from '@components/Footer'
 export default function Home() {
   return (
     <div className="container">
-      <Head>
-      </Head>
       <main><a href="https://freeimage.host/i/JgA9m7I"><img src="https://iili.io/JgA9m7I.md.png" alt="JgA9m7I.md.png" border="0"></a>
       </main>
     </div>
